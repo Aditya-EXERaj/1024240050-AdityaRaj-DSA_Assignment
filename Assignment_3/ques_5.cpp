2680d1052d56850e7e5d23d3f02ea20bf8a9d4da
AdityaRaj3347F <adityawork925@gmail.com>
2025-09-14 22:56:25 +0530

    Update ques_5.cpp

--- Assignment_3/ques_5.cpp
+++ Assignment_3/ques_5.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <stack>
 using namespace std;
-
 int evaluatePostfix(string exp) {
     stack<int> st;
 
-    for (char c : exp) {
-        if (isdigit(c))
+for (char c : exp) {
+    if (isdigit(c))
             st.push(c - '0');
-        else {
-            int val2 = st.top(); st.pop();
+    else {
+        int val2 = st.top(); st.pop();
             int val1 = st.top(); st.pop();
             switch (c) {
             case '+': st.push(val1 + val2); break;
@@ -17,7 +16,7 @@ int evaluatePostfix(string exp) {
             case '*': st.push(val1 * val2); break;
             case '/': st.push(val1 / val2); break;
             }
-        }
+      }
     }
     return st.top();
 }
